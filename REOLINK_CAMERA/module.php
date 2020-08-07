0a7adf6780a5ea9a39bf7a1aac156ea3236e0018
Volkmar Stegmann <volkmar.stegmann@sap.com>
2020-08-07 13:27:24 +0200

    Update module.php

--- REOLINK_CAMERA/module.php
+++ REOLINK_CAMERA/module.php
@@ -43,7 +43,7 @@
         } 
         
         public function Destroy() {
-            $this->UnregisterTimer("ReolinkCamera_UpdateTimer");
+            $this->RegisterTimer( "ReolinkCamera_UpdateTimer", 0 );
             // Never delete this line!
             parent::Destroy();
         }
